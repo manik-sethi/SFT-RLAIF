85fe8a953a5d522988da1f74548ff5c701af60e5
manik-sethi <sethi.manik03@gmail.com>
2025-12-29 10:13:42 +0000

    Refactor model definition

--- src/model.py
+++ src/model.py
@@ -3,44 +3,33 @@ import os
 import torch
 import torch.nn as nn
 
-class Reward_Model(nn.Module):
+class BradleyTerryRewardModel(nn.Module):
 
-    def __init__(self, model):
+    def __init__(self, base_lm):
         super().__init__()
-        self.base = model
+        self.lm = base_lm
         self.score_head = nn.Linear(
-            in_features=1024,
+            in_features=self.lm.config.hidden_size,
             out_features=1
         )
-        if hasattr(model, 'device'):
-            device = model.device
-        elif next(model.parameters(), None) is not None:
-            device = next(self.base.parameters()).device
-        else:
-            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         
-        # Match the dtype of the base model (likely bfloat16 from your config)
-        dtype = next(self.base.parameters()).dtype
+    def _sequence_rep(self, hidden, attention_mask):
+        lengths = attention_mask.sum(dim=1)-1
+        batch_idx = torch.arange(hidden.size(0), device=hidden.device)
+
+        return hidden[batch_idx, lengths]
+
+
         
-        self.score_head = self.score_head.to(device=device, dtype=dtype)
+    def forward(self, input_ids, attention_mask=None):
+        outputs = self.lm(
+            input_ids = input_ids,
+            attention_mask=attention_mask,
+            output_hidden_states=True,
+            return_dict=True,
+        )
+        hidden = outputs.hidden_states[-1]
+        seq_repr = self._sequence_rep(hidden, attention_mask)
+        rewards = self.score_head(seq_repor).squeeze(-1)
 
-    def forward(self, x, attention_mask=None):
-        hidden = self.base(x, attention_mask=attention_mask).last_hidden_state
-        pooled = hidden[:,0,:]
-        score = self.score_head(pooled)
-        return score
-        
-
-def load_model(model_name: str):
-
-    
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-    model_path = os.path.join(os.getcwd(), "..", "models")
-
-    return model
-    # model.save_pretrained(model_path)
-def load_tokenizer(model_name: str):
-    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
-    return tokenizer
+        return rewards
